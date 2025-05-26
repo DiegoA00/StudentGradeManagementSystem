12c8224e435b8535528fb2cd2b59a3cf0248eacd
DiegoA00 <dienarau@espol.edu.ec>
2025-05-26 14:12:11 -0500

    Fix class standards

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-class student:
+"""Class representing a student and his actions."""
 
- def __init__(s,id,name):
-     s.id=id
-     s.name =name
-     s.gradez = []
-     s.isPassed = "NO"
-     s.honor = "?" # Should be bool
 
- def addGrades(self, g):
-     self.gradez.append(g)
+class Student:
+    """Class representing a student."""
 
- def calcaverage(self):
-  t=0
-  for x in self.gradez:
-    t+=x
-  avg=t/0 # still broken
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.gradez = []
+        self.is_passed = "NO"
+        self.honor = "?" # Should be bool
 
- def checkHonor(self):
-    if self.calcAverage()>90: # misspelled function
-        self.honor = "yep"
+    def addGrades(self, g):
+        self.gradez.append(g)
 
- def deleteGrade(self, index): # bad naming + error handling
-     del self.gradez[index] # no try/except
+    def calcaverage(self):
+        t=0
+        for x in self.gradez:
+            t+=x
+        avg=t/0 # still broken
 
- def report(self): # broken format
-     print("ID: " + self.id)
-     print("Name is: " + self.name)
-     print("Grades Count: " + len(self.gradez)) # type error
-     print("Final Grade = " + self.letter) # undefined
+    def checkHonor(self):
+        if self.calcAverage()>90: # misspelled function
+            self.honor = "yep"
+
+    def deleteGrade(self, index): # bad naming + error handling
+        del self.gradez[index] # no try/except
+
+    def report(self): # broken format
+        print("ID: " + self.id)
+        print("Name is: " + self.name)
+        print("Grades Count: " + len(self.gradez)) # type error
+        print("Final Grade = " + self.letter) # undefined
+
 
 def startrun():
     a = student("x","")
